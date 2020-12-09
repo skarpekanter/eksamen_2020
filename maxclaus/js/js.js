421a0d143f7f5ab7f6bf68c8a0576353727d088b
simon <sjonassen1@gmail.com>
2020-12-09 23:13:05 +0100

    NYT COMMIT

--- maxclaus/js/js.js
+++ maxclaus/js/js.js
@@ -4,9 +4,3 @@
  **/
 
 console.log('JavaScript from js/js.js: up and running!');
-
-function addClass() {
-  document.body.classList.add("sent");
-}
-
-sendLetter.addEventListener("click", addClass);
